e6a2a200dcdd05b315e41c6b7d04cf885874ea1e
THELEGEND099 <prashantsmishra024@gmail.com>
2026-05-30 12:50:26 +0530

    Updates

--- _next/static/gcE37EbocW0i4Z9rPNd_3/_buildManifest.js
+++ _next/static/gcE37EbocW0i4Z9rPNd_3/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/Portfolio//_next/:path+",
-        "destination": "/Portfolio/_next/:path+"
+        "source": "/your-repository-name//_next/:path+",
+        "destination": "/your-repository-name/_next/:path+"
       }
     ],
     "fallback": []
